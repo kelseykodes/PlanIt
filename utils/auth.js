cdfeeab6f729d1dea48de543e5b75c20599a95fb
kperkins97 <perkins.kelsey@yahoo.com>
2021-12-05 18:14:28 -0600

    created files needed for authentication

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// Created auth.js file to see if the application has the use authenticated.
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
