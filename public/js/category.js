fc0a87ca32bf103d820b09845dd5a6bbbbd084a3
Jing Li <lijing900913@gmail.com>
2021-12-08 18:17:31 -0500

    modify category.js and party-form.html

--- public/js/category.js
+++ public/js/category.js
@@ -1,32 +1,32 @@
 // control the dropdown menu
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible.expandable');
-    M.Collapsible.init(elems, {accordion: false});
-  });
+  var elems = document.querySelectorAll('.collapsible.expandable');
+  M.Collapsible.init(elems, {accordion: false});
+});
 
 // control add-all button
 
 const checks = document.getElementsByClassName('checkbox');
 const currentList = document.getElementById("current-list");
-const addAll = document.getElementById("add-all")
+const addAll = document.getElementById("addBtn")
 let answer = [];
 
 function getValue(e){
-  e.preventDefault();
-  currentList.innerHTML = " ";
-  for(i=0;i<checks.length; i++){
-    if(checks[i].checked == true){
-        answer.push(checks[i].value);  
-      }
-  }
-  for(j=0;j<answer.length; j++){
-    let li = document.createElement("li");
-    li.innerHTML = answer[j];
-    currentList.appendChild(li);
-  }  
-  console.log(answer);
-  storeTheSelection = answer;
-  answer = [];
+e.preventDefault();
+currentList.innerHTML = " ";
+for(i=0;i<checks.length; i++){
+  if(checks[i].checked == true){
+      answer.push(checks[i].value);  
+    }
+}
+for(j=0;j<answer.length; j++){
+  let li = document.createElement("li");
+  li.innerHTML = answer[j];
+  currentList.appendChild(li);
+}  
+console.log(answer);
+storeTheSelection = answer;
+answer = [];
 }  
 
 addAll.addEventListener('click', getValue);
@@ -35,14 +35,14 @@ addAll.addEventListener('click', getValue);
 const clearAll = document.getElementById("clear-all");
 
 function clearValue(e){
-    e.preventDefault();
-    for(i=0;i<checks.length; i++){
-      if(checks[i].checked == true){
-        checks[i].checked = false;
-        // change answer[]
-      }
-      currentList.innerHTML = " ";
+  e.preventDefault();
+  for(i=0;i<checks.length; i++){
+    if(checks[i].checked == true){
+      checks[i].checked = false;
+      // change answer[]
     }
+    currentList.innerHTML = " ";
+  }
 }
 
 clearAll.addEventListener('click', clearValue);
@@ -53,15 +53,15 @@ const saveBtn = document.getElementById("saveBtn");
 let finalAnswer = [];
 
 const generateAnswer = (e) =>{
-  e.preventDefault();
-  // console.log(currentList.children[0].innerHTML);
-  for (i=0; i<currentList.childElementCount; i++){
-    finalAnswer.push(currentList.children[i].innerHTML);
-  }
-  console.log(finalAnswer);
-  return finalAnswer;
-  // back to the front page
-  // window location changed
+e.preventDefault();
+// console.log(currentList.children[0].innerHTML);
+for (i=0; i<currentList.childElementCount; i++){
+  finalAnswer.push(currentList.children[i].innerHTML);
+}
+console.log(finalAnswer);
+return finalAnswer;
+// back to the front page
+// window location changed
 }
 
 saveBtn.addEventListener('click', generateAnswer);
