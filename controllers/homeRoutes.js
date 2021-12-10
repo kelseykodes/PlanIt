564e8175acee3d07f924a64daf78531f9f33c4e6
Reagin Turner <rturnerafteragnes@gmail.com>
2021-12-10 08:31:25 -0500

    Attempting to render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,7 +35,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
   }
 });
 
-
+//Reagin's attempt
 // router.get('/party-form', async (req, res) => {
 //   try {
 //     const foodData = await Food.findAll();
@@ -52,37 +52,47 @@ router.get('/dashboard', withAuth, async (req, res) => {
 //   }
 // });
 
-const { Food } = require('../models');
-const { Party } = require('../models');
-const { Activities } = require('../models');
-const { Music } = require('../models');
-const { Decoration } = require('../models');
 
-router.get('/party-form', async (req, res) => {
-  try {
-    const dbFoodData = await Party.findAll({
-      include: [
-        {
-          model: Food,
-          attributes: ['name', 'has_nuts'],
-        },
-      ],
-    });
+//new code
+// const { Food } = require('../models');
+// const { Party } = require('../models');
+// const { Activities } = require('../models');
+// const { Music } = require('../models');
+// const { Decoration } = require('../models');
 
-    const parties = dbFoodData.map((party-form) =>
-      party.get({ plain: true })
-    );
+// router.get('/party-form', async (req, res) => {
+//   try {
+//     const dbFoodData = await Party.findAll({
+//       include: [
+//         {
+//           model: Food,
+//           attributes: ['name', 'has_nuts'],
+//         },
+//       ],
+//     });
 
-    res.render('food', {
-      parties,
-    });
+//     const parties = dbFoodData.map((party-form) =>
+//       party.get({ plain: true })
+//     );
 
-  } catch (err) {
-    console.log(err);
-    res.status(500) 
-    // .json(err);
-  }
-});
+//     res.render('food', {
+//       parties,
+//     });
+
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500) 
+//     // .json(err);
+//   }
+// });
+// var nameVal = $(#name)
+// var DescriptVal = $("#descript")
+// var formVal = $("#form")
+
+// app.get('/party-form', function(req, res){
+//   res.send({ form-info: formVal });
+// });
+//End of Reagin's attmept
 
 
 // GET route takes user to party form when logged in
