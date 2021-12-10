415d5d6662a45c232c8f4e740f2e1a8d89be8f7d
Jing Li <lijing900913@gmail.com>
2021-12-09 19:52:02 -0500

    Basic Combination the routes and handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const partyRoutes = require('./partyRoutes');
 
 router.use('/users', userRoutes);
+router.use('/parties', partyRoutes);
 
 module.exports = router;
